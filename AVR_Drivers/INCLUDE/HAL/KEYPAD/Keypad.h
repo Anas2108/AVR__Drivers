1af9c0f400eee9be36a0802923df80fdf1a49e0a
Anas hamed ahmed hamed <111208532+Anas2108@users.noreply.github.com>
2022-10-08 02:18:04 +0200

    Add files via upload

---
+++ AVR_Drivers/INCLUDE/HAL/KEYPAD/Keypad.h
@@ -0,0 +1,27 @@
+/*
+ * Keypad.h
+ *
+ *  Created on: Sep 10, 2022
+ *      Author: ANOOS
+ */
+
+#ifndef INCLUDE_HAL_KEYPAD_KEYPAD_H_
+#define INCLUDE_HAL_KEYPAD_KEYPAD_H_
+
+#include "../../../SERVICES/std_types.h"
+
+
+
+
+
+
+u8 Keypad_voidGetPressedKey(void);
+void Keypad_voidSetKeyPadDir();
+
+
+
+
+
+
+
+#endif /* INCLUDE_HAL_KEYPAD_KEYPAD_H_ */
